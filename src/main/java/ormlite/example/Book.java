866395504ed2fc91e2156d5555faa2f3ad7c00db
izielin <m.michal.parzych@gmail.com>
2020-09-08 14:59:00 +0200

    more precise definition of table fields

--- src/main/java/ormlite/example/Book.java
+++ src/main/java/ormlite/example/Book.java
@@ -1,7 +1,11 @@
 package ormlite.example;
 
+import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
 
 @DatabaseTable(tableName = "BOOKS")
 public class Book {
@@ -11,8 +15,25 @@ public class Book {
     @DatabaseField(generatedId = true, columnName = "ID")
     private int id;
 
-    @DatabaseField(columnName = "TITLE")
+    @DatabaseField(columnName = "TITLE", canBeNull = false)
     private String title;
+
+    @DatabaseField(columnName = "DESCRIPTION", dataType = DataType.LONG_STRING)
+    private String description;
+
+    @DatabaseField(columnName = "ISBN", unique = true)
+    private String isbn;
+
+    @DatabaseField(columnName = "ADDED_DATE")
+    private Date addedDate;
+
+    @DatabaseField(columnName = "RELEASED_DATE", dataType = DataType.DATE_STRING, format = "yyyy-MM-DD")
+    private Date releasedDate;
+
+    @DatabaseField(columnName = "RATING", width = 1)
+    private String rating;
+    @DatabaseField(columnName = "BORROWED", defaultValue = "false")
+    private boolean borrowed;
 
     @DatabaseField(columnName = "AUTHOR")
     private String author;
@@ -20,4 +41,83 @@ public class Book {
     @DatabaseField(columnName = "PRICE")
     private double price;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(Date addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public Date getReleasedDate() {
+        return releasedDate;
+    }
+
+    public void setReleasedDate(Date releasedDate) {
+        this.releasedDate = releasedDate;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/ormlite/example/Main.java
+++ src/main/java/ormlite/example/Main.java
@@ -8,19 +8,48 @@ import com.j256.ormlite.table.TableUtils;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, IOException {
-        String databaseUrlSqlite = "jdbc:sqlite:sqliteDatabase";
-        String databaseUrlH2 = "jdbc:h2:./H2DataBase";
+    private static final String JDBC_DRIVER_HD = "jdbc:h2:./databaseH2";
+    private static final String USER = "admin";
+    private static final String PASS = "admin";
+    private static JdbcConnectionSource connectionSource;
+
+    public static void main(String[] args) throws SQLException, IOException, ParseException {
 
         // create single connection
-        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrlSqlite);
-//        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrlH2, "admin", "admin");
+//        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrlSqlite);
+        try {
+            connectionSource = new JdbcConnectionSource(JDBC_DRIVER_HD, USER, PASS);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
 
         TableUtils.dropTable(connectionSource, Book.class, true);
-        TableUtils.createTableIfNotExists(connectionSource,Book.class);
+        TableUtils.createTableIfNotExists(connectionSource, Book.class);
 //        TableUtils.createTable(connectionSource, Book.class);
+
+        Book book = new Book();
+        book.setTitle("Lord of the Rings");
+        book.setAuthor("J.R.R Tolkien");
+        book.setIsbn("127151");
+        book.setAddedDate(new Date());
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyy/MM/dd");
+        String dateString = "2012/11/17";
+        Date date = simpleDateFormat.parse(dateString);
+
+        book.setReleasedDate(date);
+        book.setRating("5");
+        book.setBorrowed(true);
+        book.setPrice(34.99);
+
+        Dao<Book, ?> dao = DaoManager.createDao(connectionSource, Book.class);
+        dao.create(book);
 
         // close connection
         connectionSource.close();
