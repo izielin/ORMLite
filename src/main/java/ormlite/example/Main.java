5a51b6670682a4ff9b7e234aa12a869cf0b4b1da
izielin <m.michal.parzych@gmail.com>
2020-09-25 15:12:03 +0200

    raw queries

--- src/main/java/ormlite/example/Main.java
+++ src/main/java/ormlite/example/Main.java
@@ -2,6 +2,7 @@ package ormlite.example;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.dao.GenericRawResults;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
@@ -11,6 +12,7 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     private static final String JDBC_DRIVER_HD = "jdbc:h2:./databaseH2";
@@ -48,19 +50,51 @@ public class Main {
         book.setBorrowed(true);
         book.setPrice(34.99);
 
+        Book book2 = new Book();
+        book2.setTitle("Hobbit");
+        book2.setAuthor("J.R.R Tolkien");
+        book2.setIsbn("13327151");
+        book2.setAddedDate(new Date());
+
+        String dateString2 = "2009/01/07";
+        Date date2 = simpleDateFormat.parse(dateString2);
+
+        book2.setReleasedDate(date2);
+        book2.setRating("5");
+        book2.setBorrowed(true);
+        book2.setPrice(29.99);
+
+        Book book3 = new Book();
+        book3.setTitle("Silmarillion");
+        book3.setAuthor("J.R.R Tolkien");
+        book3.setIsbn("12776151");
+        book3.setAddedDate(new Date());
+
+        String dateString3 = "2011/12/11";
+        Date date3 = simpleDateFormat.parse(dateString3);
+
+        book3.setReleasedDate(date3);
+        book3.setRating("3");
+        book3.setBorrowed(false);
+        book3.setPrice(24.99);
+
         Dao<Book, Integer> dao = DaoManager.createDao(connectionSource, Book.class);
         dao.create(book);
-        System.out.println("Before Update: " + book);
+        dao.create(book2);
+        dao.create(book3);
 
-        book.setTitle("Hobbit");
-        dao.update(book);
-        System.out.println("After Update: " + book);
+//        GenericRawResults<String[]> rawResults = dao.queryRaw("SELECT * FROM books");
+//        GenericRawResults<String[]> rawResults = dao.queryRaw("SELECT * FROM books WHERE title = 'Hobbit'");
+//        GenericRawResults<String[]> rawResults = dao.queryRaw("SELECT MIN(price), MAX(price) FROM books ");
+        GenericRawResults<String[]> rawResults = dao.queryRaw("SELECT count(*) FROM books WHERE borrowed = true");
+        List<String[]> result = rawResults.getResults();
+        result.forEach(e->{
+            for(String string : e)
+                System.out.println(string);
+        });
 
-        dao.delete(book);
-        System.out.println("After Delete: " + book);
-
-        book = dao.queryForId(book.getId());
-        System.out.println("After Query: " + book);
+        double maxUnit = dao.queryRawValue("SELECT AVG(price) FROM books");
+        System.out.println(maxUnit);
 
         // close connection
         connectionSource.close();
